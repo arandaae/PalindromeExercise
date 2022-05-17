08460d802db83862f38f821927236cbd6cc6cf6a
Armando Aranda <arandaae19@gmail.com>
2022-05-16 22:42:41 -0500

    First commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("radar", true)] // ← you will need to put some test data and expected result here.  
+        [InlineData("words", false)] // ← you will need to put some test data and expected result here.  
+        [InlineData("Radar", true)] // ← you will need to put some test data and expected result here.  
 
+        // For example [InlineData(“racecar”, true)]
+        public void PalindromeTest(string word, bool expected)
+        {
+            //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
